2efb8ce07f2089abc79000ee99cff3d7659cdbdb
ZaNeFY <david.bzhinava1@gmail.com>
2024-01-12 23:08:57 +0400

    third commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let celsius1 = 24;
+let fahrenheit1 = 0;
+
+fahrenheit = (celsius1 * 9/5) + 32;
+document.getElementById("celsius1").innerHTML = celsius1
+document.getElementById("fahrenheit1").innerHTML = fahrenheit
+
+let celsius2 = 12;
+let fahrenheit2 = 0;
+
+fahrenheit2 = (celsius2 * 9/5) + 32;
+document.getElementById("celsius2").innerHTML = celsius2
+document.getElementById("fahrenheit2").innerHTML = fahrenheit2
